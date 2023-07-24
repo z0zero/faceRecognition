dad1197523e4e2b4cdbc6f4fb73d775134893a75
Brian Sangapta <32585806+z0zero@users.noreply.github.com>
2023-07-24 22:47:06 +0700

    Penambahan auto brighten pada pengambilan sampel gambar

--- sampel.py
+++ sampel.py
@@ -1,13 +1,29 @@
 # Import required libraries
 import cv2
 import os
+import numpy as np
 
+# Here's a function that will adjust the brightness of an image
+def adjust_brightness(image, brightness=1.0):
+    # Convert the image to the HSV color space
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Scale the V (Value/Brightness) channel
+    hsv[...,2] = cv2.convertScaleAbs(hsv[...,2], alpha=brightness)
+    # Convert the image back to the BGR color space
+    bright_image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bright_image
+
+# Here's your original function, but now it applies brightness adjustment to each frame before saving it
 def capture_images():
     # Get the user's name
     name = input("Enter Your Name: ").lower()
 
     # Create directory for images if it doesn't exist
-    path = os.path.join('images', name)
+    images_dir = 'images'
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    path = os.path.join(images_dir, name)
 
     # Check if directory already exists
     if os.path.exists(path):
@@ -30,12 +46,15 @@ def capture_images():
         if not ret:
             break
 
+        # Adjust the brightness of the frame
+        bright_frame = adjust_brightness(frame, brightness=1.5)  # Increase brightness by 50%
+
         # Display the resulting frame
-        cv2.imshow('Capture Images', frame)
+        cv2.imshow('Capture Images', bright_frame)
 
         # Save the current image to the directory
         img_name = os.path.join(path, f'{count}.jpg')
-        cv2.imwrite(img_name, frame)
+        cv2.imwrite(img_name, bright_frame)
         print(f'Creating Images.........{img_name}')
         count += 1
 
@@ -48,4 +67,4 @@ def capture_images():
     cv2.destroyAllWindows()
 
 # Call the function
-capture_images()
+capture_images() 
