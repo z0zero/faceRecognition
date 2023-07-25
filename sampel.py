bf5e75f74901ac96eaad66c5c67671f42963628f
Brian Sangapta <32585806+z0zero@users.noreply.github.com>
2023-07-25 07:22:34 +0700

    Update sampel.py

--- sampel.py
+++ sampel.py
@@ -1,70 +1,69 @@
-# Import required libraries
+# Impor pustaka yang dibutuhkan
 import cv2
 import os
 import numpy as np
 
-# Here's a function that will adjust the brightness of an image
+# Berikut adalah fungsi yang akan menyesuaikan kecerahan gambar
 def adjust_brightness(image, brightness=1.0):
-    # Convert the image to the HSV color space
+    # Ubah gambar ke ruang warna HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # Scale the V (Value/Brightness) channel
+    # Skala kanal V (Value/Kecerahan)
     hsv[...,2] = cv2.convertScaleAbs(hsv[...,2], alpha=brightness)
-    # Convert the image back to the BGR color space
+    # Ubah kembali gambar ke ruang warna BGR
     bright_image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return bright_image
 
-# Here's your original function, but now it applies brightness adjustment to each frame before saving it
 def capture_images():
-    # Get the user's name
-    name = input("Enter Your Name: ").lower()
+    # Dapatkan nama pengguna
+    name = input("Masukkan Nama Anda: ").lower()
 
-    # Create directory for images if it doesn't exist
+    # Buat direktori untuk gambar jika tidak ada
     images_dir = 'images'
     if not os.path.exists(images_dir):
         os.makedirs(images_dir)
 
     path = os.path.join(images_dir, name)
 
-    # Check if directory already exists
+    # Cek apakah direktori sudah ada
     if os.path.exists(path):
-        print("Name Already Taken")
-        name = input("Enter Your Name Again: ").lower()
+        print("Nama Sudah Digunakan")
+        name = input("Masukkan Nama Anda Lagi: ").lower()
     else:
         os.makedirs(path)
 
-    # Open the webcam
+    # Buka webcam
     cap = cv2.VideoCapture(0)
 
-    # Initialize image count
+    # Inisialisasi hitungan gambar
     count = 0
 
     while True:
-        # Capture frame-by-frame
+        # Tangkap frame-demi-frame
         ret, frame = cap.read()
 
-        # Check if frame reading was successful
+        # Cek jika pembacaan frame berhasil
         if not ret:
             break
 
-        # Adjust the brightness of the frame
-        bright_frame = adjust_brightness(frame, brightness=1.5)  # Increase brightness by 50%
+        # Sesuaikan kecerahan frame
+        bright_frame = adjust_brightness(frame, brightness=1.5)  # Tingkatkan kecerahan sebesar 50%
 
-        # Display the resulting frame
-        cv2.imshow('Capture Images', bright_frame)
+        # Tampilkan frame hasil
+        cv2.imshow('Tangkap Gambar', bright_frame)
 
-        # Save the current image to the directory
+        # Simpan gambar saat ini ke direktori
         img_name = os.path.join(path, f'{count}.jpg')
         cv2.imwrite(img_name, bright_frame)
-        print(f'Creating Images.........{img_name}')
+        print(f'Membuat Gambar.........{img_name}')
         count += 1
 
-        # Break if 'q' is pressed or we have enough images
+        # Hentikan jika tombol 'q' ditekan atau kita sudah memiliki cukup gambar
         if cv2.waitKey(1) & 0xFF == ord('q') or count > 500:
             break
 
-    # When everything is done, release the capture and close windows
+    # Ketika semuanya selesai, lepaskan tangkapan dan tutup jendela
     cap.release()
     cv2.destroyAllWindows()
 
-# Call the function
-capture_images() 
+# Panggil fungsi
+capture_images()
